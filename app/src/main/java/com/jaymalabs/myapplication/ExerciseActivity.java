8ce69623d151493ed1848153e6123af63afbcd51
LikeWhiteOnRice <joshyundt@outlook.com>
2016-05-11 15:30:26 -0700

    Added chnages to workout sequence

--- app/src/main/java/com/jaymalabs/myapplication/ExerciseActivity.java
+++ app/src/main/java/com/jaymalabs/myapplication/ExerciseActivity.java
@@ -1,8 +1,13 @@
 package com.jaymalabs.myapplication;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -20,50 +25,60 @@ public class ExerciseActivity extends AppCompatActivity{
     private TextView mNameText;
     private TextView mTimeText;
     private TextView mRepsText;
-    private Button mNextButton;
+    private TextView mNextExercise;
     private ProgressBar mWorkoutProgress;
     private int mExerciseIndex = 0;
     private int mExerciseTime;
     private long mStartTime = 0;
     private ExerciseLab exerciseLab;
     List<Exercise> exerciseList;
-    Exercise exercise;
     private boolean mBreakNext;
-//    private Timer timer;
+    private boolean mActivityRestore;
+    private Button pause;
+    private int mSeconds;
+
+    private SharedPreferences mPrefs;
+
+    static final int SAVED_SECONDS = 0;
+
+    private final String LOG_TAG = ExerciseActivity.class.getSimpleName();
 
     Handler timerHandler = new Handler();
     Runnable timerRunnable = new Runnable() {
         @Override
         public void run() {
-            long millis = System.currentTimeMillis() - mStartTime;
-            int seconds = (int) (millis / 1000);
-//            int minutes = seconds / 60;
-            seconds = mExerciseTime - seconds;
 
-            mTimeText.setText(String.format("%2d", seconds));
+            long millis = System.currentTimeMillis() - mStartTime;
+            mSeconds = (int) (millis / 1000);
+//            int minutes = seconds / 60;
+            mSeconds = mExerciseTime - mSeconds;
+
+            mTimeText.setText(String.format("%2d", mSeconds));
             timerHandler.postDelayed(this, 50);
 
-            if(seconds <= 0) {
+            Log.v(LOG_TAG, String.valueOf(mSeconds));
 
-                if(mBreakNext) {
-                    mIdText.setText("-1");
-                    mNameText.setText("Break");
-                    mExerciseTime = 10;
-                    mRepsText.setText("1");
-                    mWorkoutProgress.setProgress((mExerciseIndex + 1) *20);
-                    mStartTime = System.currentTimeMillis();
-                    timerHandler.postDelayed(timerRunnable, 0);
-
-                    mBreakNext = false;
+            if(mSeconds <= 0) {
+                if (mExerciseIndex == exerciseList.size()-1) {
+                    Log.v(LOG_TAG, "CLOSE THE ACTIVITY");
+                    closeActivity();
                 }
                 else {
-                    mExerciseIndex++;
-
-                    if (mExerciseIndex > exerciseList.size()-1) {
-                        mExerciseIndex = 0;
+                    if(mBreakNext) {
+                        mWorkoutProgress.setProgress(mWorkoutProgress.getProgress() + 20);
+                        updateUI(0);
+                        mStartTime = System.currentTimeMillis();
+                        timerHandler.postDelayed(timerRunnable, 0);
+                        mBreakNext = false;
+                        Log.v(LOG_TAG, "---------------Break Next--------------------");
                     }
-                    mBreakNext = true;
-                    updateUI();
+                    else {
+                        mWorkoutProgress.setProgress(mWorkoutProgress.getProgress() + 5);
+                        mExerciseIndex++;
+                        mBreakNext = true;
+                        updateUI(mExerciseIndex);
+                        Log.v(LOG_TAG, "---------------Else--------------------");
+                    }
                 }
             }
         }
@@ -73,10 +88,19 @@ public class ExerciseActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.v(LOG_TAG, "ON CREATE");
 
         setContentView(R.layout.activity_exercise);
 
-        mBreakNext = true;
+        if( savedInstanceState != null ) {
+            mExerciseTime = savedInstanceState.getInt("seconds");
+            mBreakNext = savedInstanceState.getBoolean("break_next");
+            mActivityRestore = true;
+        }
+
+        //get current screen orientation
+        //create lock orientation button and set orientation accordingly
+//        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
         exerciseLab = ExerciseLab.get(this);
         exerciseList = exerciseLab.getExercises();
@@ -85,42 +109,72 @@ public class ExerciseActivity extends AppCompatActivity{
         mNameText = (TextView) findViewById(R.id.exercise_name);
         mTimeText = (TextView) findViewById(R.id.exercise_time);
         mRepsText = (TextView) findViewById(R.id.exercise_reps);
-        mWorkoutProgress = (ProgressBar) findViewById(R.id.progressBar);
-        updateUI();
-        mNextButton = (Button) findViewById(R.id.next_button);
-        mNextButton.setOnClickListener(new View.OnClickListener() {
+        mNextExercise = (TextView) findViewById(R.id.next_exercise);
+
+        pause = (Button) findViewById(R.id.button4);
+        pause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //do something
-                mExerciseIndex++;
-
-                if (mExerciseIndex > exerciseList.size()-1) {
-                    mExerciseIndex = 0;
-                }
-                updateUI();
+                timerHandler.removeCallbacks(timerRunnable);
             }
         });
+        mWorkoutProgress = (ProgressBar) findViewById(R.id.progressBar);
+        updateUI(mExerciseIndex);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        timerHandler.postDelayed(timerRunnable, 0);
+        Log.v(LOG_TAG, "ON RESUME");
+        timerHandler.postDelayed(timerRunnable, 50);
     }
 
+    @Override
     public void onPause() {
         super.onPause();
+        Log.v(LOG_TAG, "ON PAUSE");
         timerHandler.removeCallbacks(timerRunnable);
     }
 
-    private void updateUI() {
-        exercise = exerciseLab.getExercise(mExerciseIndex);
+//    @Override
+//    public void onStop() {
+//        super.onStop();
+//        Log.v(LOG_TAG, "ON STOP");
+//        timerHandler.removeCallbacks(timerRunnable);
+//    }
+
+    private void updateUI(int exerciseNum) {
+        Log.v(LOG_TAG, "UPDATE UI");
+        Exercise exercise = exerciseLab.getExercise(exerciseNum);
 
         mIdText.setText(String.valueOf(exercise.getId()));
         mNameText.setText(exercise.getName());
-        mExerciseTime = (exercise.getTime());
+        if (mActivityRestore) {
+            mActivityRestore = false;
+        }
+        else{
+            mExerciseTime = (exercise.getTime());
+        }
         mRepsText.setText(String.valueOf(exercise.getReps()));
+        if (exerciseNum == 0) {
+            Exercise nextExercise = exerciseLab.getExercise(mExerciseIndex + 1);
+            mNextExercise.setText("Next Exercise: " + nextExercise.getName());
+        }
+        else {
+            mNextExercise.setText("");
+        }
         mStartTime = System.currentTimeMillis();
         timerHandler.postDelayed(timerRunnable, 0);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("seconds", mSeconds);
+        savedInstanceState.putBoolean("break_next", mBreakNext);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    private void closeActivity() {
+        this.finish();
     }
 }
--- app/src/main/java/com/jaymalabs/myapplication/ExerciseLab.java
+++ app/src/main/java/com/jaymalabs/myapplication/ExerciseLab.java
@@ -25,8 +25,8 @@ public class ExerciseLab {
 
         Exercise exercise0 = new Exercise();
         exercise0.setId(0);
-        exercise0.setName("Crunches");
-        exercise0.setTime(30);
+        exercise0.setName("Get Ready");
+        exercise0.setTime(5);
         exercise0.setReps(1);
         //exercise0.setStartImage();
         //exercise0.setEndImage();
@@ -34,30 +34,39 @@ public class ExerciseLab {
 
         Exercise exercise1 = new Exercise();
         exercise1.setId(1);
-        exercise1.setName("Planks");
-        exercise1.setTime(60);
+        exercise1.setName("Crunches");
+        exercise1.setTime(20);
         exercise1.setReps(1);
-        //exercise1.setStartImage();
-        //exercise1.setEndImage();
+        //exercise0.setStartImage();
+        //exercise0.setEndImage();
         mExercises.add(exercise1);
 
         Exercise exercise2 = new Exercise();
         exercise2.setId(2);
-        exercise2.setName("Bicycles");
-        exercise2.setTime(30);
+        exercise2.setName("Planks");
+        exercise2.setTime(10);
         exercise2.setReps(1);
-        //exercise2.setStartImage();
-        //exercise2.setEndImage();
+        //exercise1.setStartImage();
+        //exercise1.setEndImage();
         mExercises.add(exercise2);
 
         Exercise exercise3 = new Exercise();
         exercise3.setId(3);
-        exercise3.setName("Leg Ups");
-        exercise3.setTime(60);
+        exercise3.setName("Bicycles");
+        exercise3.setTime(10);
         exercise3.setReps(1);
+        //exercise2.setStartImage();
+        //exercise2.setEndImage();
+        mExercises.add(exercise3);
+
+        Exercise exercise4 = new Exercise();
+        exercise4.setId(4);
+        exercise4.setName("Leg Ups");
+        exercise4.setTime(10);
+        exercise4.setReps(1);
         //exercise3.setStartImage();
         //exercise3.setEndImage();
-        mExercises.add(exercise3);
+        mExercises.add(exercise4);
     }
 
     public List<Exercise> getExercises() {
